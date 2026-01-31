fd0a9c85f91a4a5933bbd92c3ca4cdb1f4bbead1
Smit Bharvadiya <smitbharvadiya@gmail.com>
2026-01-31 11:15:47 +0530

    updated webhook normalization logic

--- backend/src/adapters/stripeWebhookAdaptor.ts
+++ backend/src/adapters/stripeWebhookAdaptor.ts
@@ -6,7 +6,7 @@ export class StripeWebhookAdapter implements WebhookAdapter {
 
     verifyWebhook(req: any, secret: any) {
         const webhookBody = req.body;
-        const webhookSignature  = req.headers["stripe-signature"] as string;
+        const webhookSignature = req.headers["stripe-signature"] as string;
         const webhookSecret = secret as string;
 
         let event: Stripe.Event;
@@ -24,48 +24,42 @@ export class StripeWebhookAdapter implements WebhookAdapter {
     async normalizeWebhook(event: Stripe.Event) {
         console.log("Stripe event received:", event.type);
 
+        if (!event.type.startsWith("payment_intent.")) {
+            console.log("Ignoring non payment_intent event");
+            return;
+        }
 
         const paymentIntent = event.data.object as Stripe.PaymentIntent;
 
-        let status:
-            | "CREATED"
-            | "ATTEMPTED"
-            | "AUTHORIZED"
-            | "CAPTURED"
-            | "FAILED"
-            = "ATTEMPTED";
+        if (!paymentIntent.metadata?.order_id) {
+            console.warn("order_id missing in metadata");
+            return;
+        }
+
+        let status: "ATTEMPTED" | "CREATED" | "CAPTURED" | "FAILED" = "ATTEMPTED";
 
         switch (event.type) {
             case "payment_intent.succeeded":
                 status = "CAPTURED";
-                await paymentModel.findOneAndUpdate(
-                    { orderId: paymentIntent.metadata.order_id },
-                    {
-                        paymentId: paymentIntent.id,
-                        status,
-                        amount: paymentIntent.amount_received,
-                        raw: event,
-                    });
-                console.log("Payment updated in DB!");
                 break;
-
             case "payment_intent.payment_failed":
                 status = "FAILED";
-                await paymentModel.findOneAndUpdate(
-                    { orderId: paymentIntent.metadata.order_id },
-                    {
-                        paymentId: paymentIntent.id,
-                        status,
-                        amount: paymentIntent.amount_received,
-                        raw: event,
-                    }
-                );
-                console.log("payment.failed saved");
                 break;
-
             default:
                 console.log("Unhandled event:", event.type);
         }
+
+        await paymentModel.findOneAndUpdate(
+            { orderId: paymentIntent.metadata.order_id },
+            {
+                paymentId: paymentIntent.id,
+                status,
+                amount: paymentIntent.amount_received,
+                raw: event,
+            }
+        );
+
+        console.log("Payment updated:", status);
 
         return {
             provider: "stripe",
